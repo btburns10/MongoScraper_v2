7ed82d0d06d859d0716336729b13ac56f034fe8e
Burnsy <btburns10@gmail.com>
2018-11-02 17:37:54 -0400

    comment component init

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,25 +1,48 @@
 import React, { Component } from "react";
+import { Modal, Button } from "react-materialize";
 import axios from "axios";
 
 class Comment extends Component {
 
  state = {
-   comments: []
+   comment: []
  }
 
   componentDidMount() {
-    axios.get("/api/articles/" + this.state.comments[0])
-      .then(res => this.setState({ comments: res.data }))
-      .catch(err => console.log(err))
+    // axios.get("/api/articles/" + this.state.comments[0])
+    //   .then(res => this.setState({ comments: res.data }))
+    //   .catch(err => console.log(err))
   }
 
   render() {
     return(
       <div>
-        comments
+      <Modal
+        header={this.props.article.title}
+        fixedFooter
+        trigger={<button data-id={this.props.article_id} className="btn primary right">
+                  <i className="large material-icons">comment</i>
+                </button>}
+        actions={
+                  <div>
+                    <Button className="teal" flat modal="close" waves="light" style={{color: "white"}}>Save</Button>
+                  </div>
+                }
+        >
+        <textarea style={{height: 250}}
+        onChange={this.props.handleInputChange}
+        value={this.props.note}
+        name="note"
+        type="text"
+        className="form-control"
+        placeholder="type notes here"
+        id="noteModal"
+      />
+      </Modal>
       </div>
     )
   }
+
   
 };
 
--- src/components/SavedArticle.js
+++ src/components/SavedArticle.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal } from "react-materialize";
+import Comment from "./Comment";
 import "../css/SavedArticle.css";
 
 
@@ -8,7 +8,7 @@ const SavedArticle = props => (
 <div className="col s12 m12 l8">
   <div className="card horizontal">
     <div className="card-image">
-      <img src={props.article.img} style={{height: 250, width: 300, padding: 10}}/>
+      <img src={props.article.img} alt={props.article.title} style={{height: 250, width: 300, padding: 10}}/>
     </div>
     <div className="card-stacked">
       <div className="card-content">
@@ -17,15 +17,8 @@ const SavedArticle = props => (
         <p>{props.article.summary}</p>
       </div>
       <div className="card-action">
-        <a href={props.article.link} target="_blank">Read more here</a>
-        <Modal
-          header='Modal Header'
-          fixedFooter
-          trigger={<button data-id={props.article_id} className="btn primary right">
-                    <i className="large material-icons">comment</i>
-                  </button>}>
-          Make Notes Here!
-        </Modal>
+        <a href={props.article.link} target="_blank" rel="noopener noreferrer">Read more here</a>
+        <Comment article={props.article}/>
       </div>
     </div>
   </div>
